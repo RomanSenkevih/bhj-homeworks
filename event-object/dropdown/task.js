ec74c14e28484652781bc39ef2ff1a9e72343a56
roman <gosahorosij328@gmail.com>
2023-05-12 18:22:53 +0300

    устранил земечание ПРЕПОДАВАТЕЛЯ

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -2,7 +2,7 @@ const dropdownList = document.querySelector('.dropdown__list');
 const dropdownValue = document.querySelector('.dropdown__value');
 const dropdownItem = Array.from(document.querySelectorAll('.dropdown__item'));
 
-function onclickValue() { dropdownList.classList.add('dropdown__list_active')};
+function onclickValue() { dropdownList.classList.toggle('dropdown__list_active')};
 dropdownValue.addEventListener('click', onclickValue);
 
 function onclickItem(event) {
