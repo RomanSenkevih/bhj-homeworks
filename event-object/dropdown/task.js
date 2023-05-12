80bb5594ce697af76a3f19ded94bb9f9108a926b
roman <gosahorosij328@gmail.com>
2023-05-12 13:46:33 +0300

    Выпадающие списки готовы

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,15 @@
+const dropdownList = document.querySelector('.dropdown__list');
+const dropdownValue = document.querySelector('.dropdown__value');
+const dropdownItem = Array.from(document.querySelectorAll('.dropdown__item'));
+
+function onclickValue() { dropdownList.classList.add('dropdown__list_active')};
+dropdownValue.addEventListener('click', onclickValue);
+
+function onclickItem(event) {
+    dropdownValue.textContent = event.target.textContent;
+    dropdownList.classList.remove('dropdown__list_active')
+    return false;
+};
+dropdownItem.forEach(element => element.onclick = onclickItem);
+
+
