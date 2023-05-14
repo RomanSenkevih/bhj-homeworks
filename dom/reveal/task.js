8127d931f499edae46a36b78d8cb96f8437c1854
roman <gosahorosij328@gmail.com>
2023-05-14 21:07:12 +0300

    исправил замечание преподователя

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,12 +1,12 @@
 const reveal = document.querySelector('.reveal');
 
-function isVisible() {
+window.addEventListener('scroll',function isVisible() {
     const {top, bottom} = reveal.getBoundingClientRect();
-    if(bottom < 0 || top > window.innerHeight){
+    if(bottom < 100 || top > (window.innerHeight - 100)){
       reveal.classList.remove('reveal_active')
     }else{
         reveal.classList.add('reveal_active')
     }
-};
- setInterval(isVisible, 1000);
+});
+
 
