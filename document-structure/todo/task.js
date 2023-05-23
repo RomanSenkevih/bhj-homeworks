1523c15711a81af7d74bf44db4ff50299df333f8
roman <gosahorosij328@gmail.com>
2023-05-23 21:18:53 +0300

    Простой список дел готов

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,28 +1,25 @@
-const tasks__add = document.querySelector('.tasks__add');
-const tasks__input = document.querySelector('.tasks__input');
-const tasks__list = document.querySelector('.tasks__list');
+const tasksAdd = document.querySelector('.tasks__add');
+const tasksInput = document.querySelector('.tasks__input');
+const tasksList = document.querySelector('.tasks__list');
 
-tasks__add.addEventListener('click', (event)  => {
+tasksAdd.addEventListener('click', (event)  => {
   event.preventDefault();
-
-  if(tasks__input.value != ''){
-  tasks__list.innerHTML += `
-    <div class="task">
-       <div class="task__title">
-          ${tasks__input.value}
-       </div>
-       <a href="#" class="task__remove">&times;</a>
-    </div>`;
-  tasks__input.value = '';
-  removesAnElement();
+  if(tasksInput.value.trim() != ''){
+  tasksList.insertAdjacentHTML('afterbegin', `
+          <div class="task">
+              <div class="task__title">
+                  ${tasksInput.value}
+              </div>
+              <a href="#" class="task__remove">&times;</a>
+          </div>`
+          );
+  tasksInput.value = '';
   };
 });
 
-function removesAnElement() {
-    let task__remove = Array.from(document.querySelectorAll('.task__remove'));
-    task__remove.forEach((el) => {
-        el.addEventListener('click', () => {
-            el.parentElement.classList.add('remuve');
-        });
-    });
-};
+tasksList.addEventListener("click", e => {
+    console.log(e.target)
+    if (e.target.classList.contains("task__remove")) {
+      e.target.parentElement.remove();
+    }
+});
